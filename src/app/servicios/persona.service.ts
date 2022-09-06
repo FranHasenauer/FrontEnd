f58cd7587492e00642c46d374d59b2e9a30cc0bb
FranHasenauer <franciscohasenauer@gmail.com>
2022-09-06 09:12:46 -0300

    Implementacion de nombre, apellido e imagen

--- src/app/servicios/persona.service.ts
+++ src/app/servicios/persona.service.ts
@@ -13,6 +13,6 @@ URL = 'Http://localhost:8080/personas/';
   constructor(private http: HttpClient) { }
 
   public getPersona():Observable<persona>{
-     return this.http.get<persona>(this.URL+'traer/perfil');
+     return this.http.get<persona>(this.URL+ 'traer/perfil');
   }
 }
